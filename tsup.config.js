2940e53d4aeba36b565f3368ca69daa115a341b8
pratiqdev <pratiqdev@gmail.com>
2023-04-11 15:12:27 -0400

    Removed "tsup" entry from package.json and now using tsup.config.js with conditional config.

--- tsup.config.js
+++ tsup.config.js
@@ -1,11 +1,30 @@
-export default {
-  entryPoints: ['./src/main.ts'],
-  format: ['cjs', 'esm'],
-  minify: false,
-  sourcemap: false,
-  outDir: './dist',
-  dts: true,
-  dtsBundleOutFile: 'types.d.ts',
-  dtsBundle: true,
-  clean: true,
-}
+// export default {
+//   entryPoints: ['./src/main.ts'],
+//   format: ['cjs'],
+//   outDir: './dist',
+//   dts: true,
+//   dtsBundle: true,
+//   dtsBundleOutFile: 'types.d.ts',
+//   sourcemap: false,
+//   minify: false,
+//   clean: true,
+//   legacyOutput: true
+// }
+
+import { defineConfig } from 'tsup'
+
+export default defineConfig((options) => {
+  return {
+    // minify: !options.watch,
+    entryPoints: ['./src/main.ts'],
+    format: ['cjs'],
+    outDir: './dist',
+    dts: true,
+    dtsBundle: true,
+    dtsBundleOutFile: 'types.d.ts',
+    sourcemap: false,
+    minify: false,
+    clean: true,
+    legacyOutput: true
+  }
+})
